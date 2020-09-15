736bdaf447a4c82509b35370fbdd3464161c76b9
Ariel Castilla <fernandoarielcastilla@gmail.com>
2020-09-15 02:44:46 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,32 +1,76 @@
 package main
 
-import (
-	"fmt"
-	//"strings"
-	//"os/exec"
-	//"encoding/csv"
-	//"log"
-	"os"
-	//"io"
-	"time"
-	//"github.com/kardianos/service"
+// http://localhost:10000/all
 
+// {
+    // "Id": "3", 
+    // "Direccion": "Brown 555", 
+    // "Latitud": "888888", 
+    // "Longitud": "999999" 
+// }
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	// "io/ioutil"
 )
 
-func doStuff() {
-			f, err := os.Create("ServiceTestData.txt")
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			
-			for {
-				f.WriteString("Hello World" + fmt.Sprintln())
-				time.Sleep(1 * time.Second)
-			}
- }
- 
- 
-func main() {  
-	 doStuff()	
+var Sucursales []Sucursal
+
+type Sucursal struct {
+    Id string `json:"Id"`
+    Direccion string `json:"Direccion"`
+    Latitud string `json:"Latitud"`
+	Longitud string `json:"Longitud"`
+}
+
+func handleRequests() {
+    myRouter := mux.NewRouter().StrictSlash(true)
+    myRouter.HandleFunc("/", homePage)
+    myRouter.HandleFunc("/all", returnAllSucursales)
+	myRouter.HandleFunc("/sucursal", createNewSucursal).Methods("POST")
+	myRouter.HandleFunc("/sucursal/{id}", returnSingleSucursal)
+    log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func returnSingleSucursal(w http.ResponseWriter, r *http.Request){
+    vars := mux.Vars(r)
+    key := vars["id"]
+
+    // fmt.Fprintf(w, "Key: " + key)
+	for _, fsucursal := range Sucursales {
+        if fsucursal.Id == key {
+            json.NewEncoder(w).Encode(fsucursal)	
+        }
+    }
+}
+
+func createNewSucursal(w http.ResponseWriter, r *http.Request) {
+    // reqBody, _ := ioutil.ReadAll(r.Body)
+    // fmt.Fprintf(w, "%+v", string(reqBody))
+	fmt.Fprintf(w, "%+v", "A ver si anda ")
+}
+
+func homePage(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Welcome to the HomePage!")
+    fmt.Println("Endpoint Hit: homePage")
+}
+
+func returnAllSucursales(w http.ResponseWriter, r *http.Request){
+    fmt.Println("Endpoint Hit: returnAllSucursales")
+    json.NewEncoder(w).Encode(Sucursales)
+}
+
+func main() {
+	fmt.Println("Rest API v2.0 - Mux Routers")
+	
+	Sucursales = []Sucursal{
+        Sucursal{Id: "1", Direccion: "Av Fleming 123", Latitud: "444444", Longitud: "111111"},
+        Sucursal{Id: "2", Direccion: "Av San Martin 123", Latitud: "555555", Longitud: "222222"},
+    }
+
+    handleRequests()
 }
